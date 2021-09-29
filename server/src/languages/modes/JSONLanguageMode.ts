d87c152e04c242cfe07cfaf19e58244342d6155e
蒋东 <852778359@qq.com>
2021-09-29 10:15:04 +0800

    feat: json的代码自动补充

--- server/src/languages/modes/JSONLanguageMode.ts
+++ server/src/languages/modes/JSONLanguageMode.ts
@@ -1,6 +1,7 @@
 import { ILanguageMode } from './ILanguageMode';
-import { CompletionItem, CompletionItemKind, Position } from 'vscode-languageserver-types';
+import { CompletionItem, CompletionItemKind, InsertTextFormat, Position, TextEdit } from 'vscode-languageserver-types';
 import { CompletionContext, CompletionParams } from 'vscode-languageserver/node';
+import { Range } from 'vscode-css-languageservice';
 import { TextDocument } from 'vscode-languageserver-textdocument';
 
 /**
@@ -31,15 +32,30 @@ export class JSONLanguageMode implements ILanguageMode {
 	];
 	
 	doComplete(document: TextDocument, position: Position, context: CompletionContext | undefined): CompletionItem[] {
+		const offset = document.offsetAt(position);
+		const lines = document.getText().slice(0, offset).split('\n');
+		const currentLine = lines[position.line].replace(/^(\t|\n|\r|\s)*/g, '');
 		const trigger = context?.triggerCharacter;
-		
+
 		if (trigger === '"') {
+			const replacementRange = Range.create(
+				document.positionAt(offset - currentLine.length),
+				document.positionAt(offset + 1), // 输入 " 会出现 "", 所以要将'""' 替换掉
+			);
 			return this.fields.map((field) => {
+				let insertText = '"' + field + '"' + ': ${0}';
+				if (field === 'singlePage') {
+					insertText = '"' + field + '"' + ': {\n\t${0}\n}';
+				}
+				
 				return {
 					label: field,
 					kind: CompletionItemKind.Field,
+					filterText: insertText,
+					insertTextFormat: InsertTextFormat.Snippet,
+					textEdit: TextEdit.replace(replacementRange, insertText)
 				};
-			});	
+			});
 		}
 		return [];
 	}
